c496669f591e0ade3e85267bfecf29df11c0520b
Andrei Solntsev <drusha49@hotmail.com>
2011-03-23 20:59:19 +0200

    Load all days by one select. NB! The select is correct, but it doesn't work via JPA.

--- app/models/ParticipationResult.java
+++ app/models/ParticipationResult.java
@@ -7,47 +7,81 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import java.util.*;
 
-public class ParticipationResult {
-	Constituency constituency;
-	Map<Integer, Integer> votesPerDay;
+@Entity
+@NamedQueries({
+		@NamedQuery(name = "getParticipation",
+				query = "SELECT new data.ParticipationResult(c,\n" +
+						" (select count(*) from vote v where c.id = v.constituencyId and v.date <= :votingDate - 6),\n" +
+						" (select count(*) from vote v where c.id = v.constituencyId and v.date <= :votingDate - 5),\n" +
+						" (select count(*) from vote v where c.id = v.constituencyId and v.date <= :votingDate - 4),\n" +
+						" (select count(*) from vote v where c.id = v.constituencyId and v.date <= :votingDate + 0.5),\n" +
+						" (select count(*) from vote v where c.id = v.constituencyId and v.date <= :votingDate + 0.6),\n" +
+						" (select count(*) from vote v where c.id = v.constituencyId and v.date <= :votingDate + 0.8),\n" +
+						" (select count(*) from vote v where c.id = v.constituencyId)\n" +
+						" ) FROM constituency c")
+})
+public class ParticipationResult extends Model {
+	private Constituency constituency;
+	private int votes6DaysBefore;
+	private int votes5DaysBefore;
+	private int votes4DaysBefore;
+	private int votesAt12OClock;
+	private int votesAt16OClock;
+	private int votesAt20OClock;
+	private int totalVotes;
 
-	public ParticipationResult(Constituency constituency) {
+	public ParticipationResult(Constituency constituency, int votes6DaysBefore, int votes5DaysBefore, int votes4DaysBefore, int votesAt12OClock, int votesAt16OClock, int votesAt20OClock, int totalVotes) {
 		this.constituency = constituency;
-		this.votesPerDay = new TreeMap<Integer,Integer>();
+		this.votes6DaysBefore = votes6DaysBefore;
+		this.votes5DaysBefore = votes5DaysBefore;
+		this.votes4DaysBefore = votes4DaysBefore;
+		this.votesAt12OClock = votesAt12OClock;
+		this.votesAt16OClock = votesAt16OClock;
+		this.votesAt20OClock = votesAt20OClock;
+		this.totalVotes = totalVotes;
 	}
 
 	public Constituency getConstituency() {
 		return constituency;
 	}
 
-	public void setDayVotes(int dayOfYear, int votes) {
-		votesPerDay.put(dayOfYear, votes);
+	public int getVotes6DaysBefore() {
+		return votes6DaysBefore;
 	}
 
-	public void addDayVotes(int dayOfYear, int votes) {
-		int oldValue = votesPerDay.containsKey(dayOfYear) ? votesPerDay.get(dayOfYear) : 0;
-		votesPerDay.put(dayOfYear, oldValue + votes);
+	public int getVotes5DaysBefore() {
+		return votes5DaysBefore;
 	}
 
-	public Map<Integer, Integer> getVotesPerDay() {
-		return votesPerDay;
+	public int getVotes4DaysBefore() {
+		return votes4DaysBefore;
+	}
+
+	public int getVotesAt12OClock() {
+		return votesAt12OClock;
+	}
+
+	public int getVotesAt16OClock() {
+		return votesAt16OClock;
+	}
+
+	public int getVotesAt20OClock() {
+		return votesAt20OClock;
+	}
+
+	public int getTotalVotes() {
+		return totalVotes;
 	}
 
 	public float[] getParticipationPerDay() {
-		Collection<Integer> votes = votesPerDay.values();
-		float[] participationPerDay = new float[votes.size()];
-		int i=0;
-		for (Integer votesPerDay : votes) {
-			participationPerDay[i++] = 100f * votesPerDay / constituency.getPopulation();
-		}
-		return participationPerDay;
-	}
-
-	public float getTotalParticipation() {
-		float totalParticipation = 0;
-		for (Float participationPerDay : getParticipationPerDay()) {
-			totalParticipation += participationPerDay;
-		}
-		return totalParticipation;
+		return new float[]{
+				votes6DaysBefore / constituency.getPopulation(),
+				votes5DaysBefore / constituency.getPopulation(),
+				votes4DaysBefore / constituency.getPopulation(),
+				votesAt12OClock / constituency.getPopulation(),
+				votesAt16OClock / constituency.getPopulation(),
+				votesAt20OClock / constituency.getPopulation(),
+				totalVotes / constituency.getPopulation()
+		};
 	}
 }
