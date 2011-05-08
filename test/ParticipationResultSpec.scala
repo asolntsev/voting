fc0118acf81b67969afc87e1997b0a09594012f5
Andrei Solntsev <drusha49@hotmail.com>
2011-05-08 19:57:55 +0300

    Try to create test with ScalaTest

---
+++ test/ParticipationResultSpec.scala
@@ -0,0 +1,38 @@
+import models.{County, ParticipationResult}
+import org.junit.Assert._
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+import scala.collection.mutable.Stack
+
+class ParticipationResultSpec extends Spec with ShouldMatchers {
+
+  describe("A Stack") {
+
+    describe("(when empty)") {
+
+      val stack = new Stack[Int]
+
+      it("should be empty") {
+        stack should be('empty)
+      }
+
+      it("should complain when popped") {
+        evaluating {
+          stack.pop()
+        } should produce[NoSuchElementException]
+      }
+    }
+  }
+
+  describe("ParticipationResult") {
+    val county1 = new County(1, "Tartumaa", 50)
+
+    it ("counts participation in percents") {
+      var votesPerDate : Array[java.lang.Integer] = Array(10, 20, 30, 40, 50)
+      var participation = new ParticipationResult(county1, votesPerDate)
+
+      var expectedParticipationPerDate : Array[Float] = Array(20, 40, 60, 80, 100)
+      assertArrayEquals(expectedParticipationPerDate, participation.getParticipationPerDay, 0.00001f)
+    }
+  }
+}
