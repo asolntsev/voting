039968ed024771089d4953c9ee59e9b70ad0102d
unknown <Andrei@.(none)>
2011-05-09 23:32:37 +0300

    Added 2 candidates for each party

--- app/data/changelog/004.candidate-data.sql
+++ app/data/changelog/004.candidate-data.sql
@@ -1,4 +1,8 @@
-INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (1, 1, 'Andrus Ansip', 507, 'Tartu linn');
-INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (2, 1, 'Urmas Paet', 497, 'Tallinn');
-INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (3, 2, 'Edgar Savisaar', 609, 'Harjumaa');
-INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (4, 2, 'Mihhail Stalnuhhin', 664, 'Tartu');
+INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (1, 3, 'Andrus Ansip', 507, 'Tartu linn');
+INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (2, 3, 'Urmas Paet', 497, 'Tallinn');
+INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (3, 4, 'Edgar Savisaar', 609, 'Harjumaa');
+INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (4, 4, 'Mihhail Stalnuhhin', 664, 'Tartu');
+INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (5, 1, 'Mart Laar', 257, 'Viljandi');
+INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (6, 1, 'Juhan Parts', 234, 'Tallinn');
+INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (7, 2, 'Sven Mikser', 423, 'Tartu');
+INSERT INTO candidate(id, partyId, name, registerNumber, bornPlace) VALUES (8, 2, 'Urve Palo', 382, 'Haapsalu');
